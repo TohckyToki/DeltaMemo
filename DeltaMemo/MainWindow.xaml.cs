a83b5bb0fdfc5b16c85ba02c5996a5810fac7201
YuengFu <YuengFu.v@outlook.com>
2021-11-07 03:09:20 +0800

    初版完成

--- DeltaMemo/MainWindow.xaml.cs
+++ DeltaMemo/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +14,89 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.IconPacks;
 
 namespace DeltaMemo
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
+        private MainWinfowModel model { get; set; }
+        private bool isClosed { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            model = this.DataContext as MainWinfowModel;
+        }
+
+        private void AddWindow(object sender, RoutedEventArgs e)
+        {
+            var window = new MainWindow();
+            window.Show();
+        }
+
+        private void SwitschDisplay(object sender, RoutedEventArgs e)
+        {
+            this.ShowInTaskbar = !this.ShowInTaskbar;
+            model.Display = this.ShowInTaskbar ? PackIconMaterialKind.EyeOutline.ToString() : PackIconMaterialKind.EyeOffOutline.ToString();
+        }
+
+        private void SwitschInTop(object sender, RoutedEventArgs e)
+        {
+            this.Topmost = !this.Topmost;
+            model.InTop = this.Topmost ? PackIconMaterialKind.PinOutline.ToString() : PackIconMaterialKind.PinOffOutline.ToString();
+        }
+
+        private void IncreaseLineHeight(object sender, RoutedEventArgs e)
+        {
+            if (model.LineHeight < 10)
+            {
+                model.LineHeight++;
+            }
+        }
+
+        private void DecreaseLineHeight(object sender, RoutedEventArgs e)
+        {
+            if (model.LineHeight > 1)
+            {
+                model.LineHeight--;
+            }
+        }
+
+        private void ToggleToolbar(object sender, RoutedEventArgs e)
+        {
+            if (model.IsShowToolbar == Visibility.Visible)
+            {
+                model.IsShowToolbar = Visibility.Collapsed;
+                model.ToggleToolbarButtonText = PackIconMaterialKind.ChevronUp.ToString();
+            }
+            else
+            {
+                model.IsShowToolbar = Visibility.Visible;
+                model.ToggleToolbarButtonText = PackIconMaterialKind.ChevronDown.ToString();
+            }
+        }
+
+        private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var text = new TextRange(mainRTB.Document.ContentStart, mainRTB.Document.ContentEnd).Text;
+            if (text.Length > 0 && isClosed == false)
+            {
+                Dialog.IsOpen = true;
+                e.Cancel = true;
+            }
+        }
+
+        private void Dialog_DialogClosing(object sender, MaterialDesignThemes.Wpf.DialogClosingEventArgs eventArgs)
+        {
+            if (((bool?)eventArgs.Parameter) ?? false)
+            {
+                this.isClosed = true;
+                this.Close();
+            }
         }
     }
 }
---
+++ DeltaMemo/MainWinfowModel.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MahApps.Metro.IconPacks;
+using System.ComponentModel;
+using System.Windows;
+
+namespace DeltaMemo
+{
+    public class MainWinfowModel : INotifyPropertyChanged
+    {
+        private string _dispaly = PackIconMaterialKind.EyeOutline.ToString();
+
+        public string Display
+        {
+            get
+            {
+                return _dispaly;
+            }
+            set
+            {
+                _dispaly = value;
+                NotifyPropertyChanged(nameof(Display));
+            }
+        }
+
+        private string _inTop = PackIconMaterialKind.PinOffOutline.ToString();
+
+        public string InTop
+        {
+            get
+            {
+                return _inTop;
+            }
+            set
+            {
+                _inTop = value;
+                NotifyPropertyChanged(nameof(InTop));
+            }
+        }
+
+        private int _lineHeight = 1;
+
+        public int LineHeight
+        {
+            get
+            {
+                return _lineHeight;
+            }
+            set
+            {
+                _lineHeight = value;
+                if (value == 1)
+                {
+                    CanIncreaseLineHeight = true;
+                    CanDecreaseLineHeight = false;
+                }
+                if (value == 10)
+                {
+                    CanIncreaseLineHeight = false;
+                    CanDecreaseLineHeight = true;
+                }
+                NotifyPropertyChanged(nameof(LineHeight));
+            }
+        }
+
+        private bool _canIncreaseLineHeight = true;
+
+        public bool CanIncreaseLineHeight
+        {
+            get
+            {
+                return _canIncreaseLineHeight;
+            }
+            set
+            {
+                _canIncreaseLineHeight = value;
+                NotifyPropertyChanged(nameof(CanIncreaseLineHeight));
+            }
+        }
+
+        private bool _canDecreaseLineHeight;
+
+        public bool CanDecreaseLineHeight
+        {
+            get
+            {
+                return _canDecreaseLineHeight;
+            }
+            set
+            {
+                _canDecreaseLineHeight = value;
+                NotifyPropertyChanged(nameof(CanDecreaseLineHeight));
+            }
+        }
+
+        private Visibility _isShowToolbar = Visibility.Visible;
+
+        public Visibility IsShowToolbar
+        {
+            get
+            {
+                return _isShowToolbar;
+            }
+            set
+            {
+                _isShowToolbar = value;
+                NotifyPropertyChanged(nameof(IsShowToolbar));
+            }
+        }
+
+        private string _toggleToolbarButtonText = PackIconMaterialKind.ChevronDown.ToString();
+
+        public string ToggleToolbarButtonText
+        {
+            get
+            {
+                return _toggleToolbarButtonText;
+            }
+            set
+            {
+                _toggleToolbarButtonText = value;
+                NotifyPropertyChanged(nameof(ToggleToolbarButtonText));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
