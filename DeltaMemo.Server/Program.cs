a112ff2de14d22d824987931a286eeda7e161538
YuengFu <YuengFu.v@outlook.com>
2021-12-20 23:10:46 +0800

    添加miniapi

--- DeltaMemo.Server/Program.cs
+++ DeltaMemo.Server/Program.cs
@@ -1,25 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+using DeltaMemo.Server;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<DeltaMemoContext>(opt => opt.UseMySql(builder.Configuration["DeltaMemo:DbConnection"], Microsoft.EntityFrameworkCore.ServerVersion.Parse(builder.Configuration["DeltaMemo:DbVersion"])));
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Add services to the container.
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+app.MapGet("/Contents", async (DeltaMemoContext db) => await db.Contents.ToListAsync());
+
+app.MapGet("/Contents/{id}", async (int id, DeltaMemoContext db) =>
+    await db.Contents.FindAsync(id)
+        is Content content
+            ? Results.Ok(content)
+            : Results.NotFound());
+
+app.MapPost("/Contents", async (Content content, DeltaMemoContext db) =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    db.Contents.Add(content);
+    await db.SaveChangesAsync();
 
-app.UseHttpsRedirection();
+    return Results.Ok();
+});
 
-app.UseAuthorization();
+app.MapPut("/Contents/{id}", async (int id, Content inputContent, DeltaMemoContext db) =>
+{
+    var content = await db.Contents.FindAsync(id);
 
-app.MapControllers();
+    if (content is null) return Results.NotFound();
+
+    content.Text = inputContent.Text;
+
+    await db.SaveChangesAsync();
+
+    return Results.Ok();
+});
+
+app.MapDelete("/Contents/{id}", async (int id, DeltaMemoContext db) =>
+{
+    if (await db.Contents.FindAsync(id) is Content content)
+    {
+        db.Contents.Remove(content);
+        await db.SaveChangesAsync();
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
+});
 
 app.Run();
