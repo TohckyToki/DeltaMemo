24ed5c74edd8d9ff2496f212fd810c1fc041ca8b
YuengFu <YuengFu.v@outlook.com>
2021-12-20 23:10:32 +0800

    添加db

---
+++ DeltaMemo.Server/Content.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DeltaMemo.Server
+{
+    public partial class Content
+    {
+        public long Id { get; set; }
+        public string Text { get; set; } = null!;
+    }
+}
---
+++ DeltaMemo.Server/DeltaMemoContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DeltaMemo.Server
+{
+    public partial class DeltaMemoContext : DbContext
+    {
+        public DeltaMemoContext()
+        {
+        }
+
+        public DeltaMemoContext(DbContextOptions<DeltaMemoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Content> Contents { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8_bin")
+                .HasCharSet("utf8");
+
+            modelBuilder.Entity<Content>(entity =>
+            {
+                entity.ToTable("Content");
+
+                entity.Property(e => e.Text).HasColumnType("text");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
