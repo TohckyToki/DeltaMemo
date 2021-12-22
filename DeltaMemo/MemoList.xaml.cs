6c0ee7dac4e08098bd1c4cb40ffda22fc5575492
YuengFu <YuengFu.v@outlook.com>
2021-12-22 23:57:10 +0800

    绑定列表窗口数据

--- DeltaMemo/MemoList.xaml.cs
+++ DeltaMemo/MemoList.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace DeltaMemo
 {
@@ -21,9 +24,30 @@ namespace DeltaMemo
     /// </summary>
     public partial class MemoList : MetroWindow
     {
+        private MemoListModel model { get; set; }
+
         public MemoList()
         {
             InitializeComponent();
+            model = this.DataContext as MemoListModel;
+        }
+
+        private async void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var http = new HttpClient();
+            var a = await http.GetAsync("http://localhost:5000/contents");
+            if (a.IsSuccessStatusCode)
+            {
+                var s = await a.Content.ReadAsStringAsync();
+                var list = JsonSerializer.Deserialize<List<Content>>(s);
+                model.Contents = list;
+            }
+        }
+
+        private void SwitchDisplay(object sender, RoutedEventArgs e)
+        {
+            model.IsOption = model.IsList;
+            model.IsList = !model.IsOption;
         }
     }
 }
