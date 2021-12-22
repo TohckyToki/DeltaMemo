b58de3a92b2fcd1af008ae9528590f96a8569c04
YuengFu <YuengFu.v@outlook.com>
2021-12-22 23:55:53 +0800

    添加json反序列化类

---
+++ DeltaMemo/Content.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace DeltaMemo
+{
+    public class Content
+    {
+        public long id { get; set; }
+        public string wroteText { get; set; }
+        public DateTime wroteDate { get; set; }
+    }
+}
